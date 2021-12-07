c36592155a5a4c0ba7040edb43ca546c1cb18559
MonterrosaF <felipe.monterrosa@hotmail.com>
2021-12-07 02:04:23 -0500

    LOGIN - LINKING HTML WITH LOGIN SERVICE

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from './services/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { BlockUI, NgBlockUI } from 'ng-block-ui';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -10,12 +11,6 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  user: User = {
-    id: 1,
-    name: 'hola',
-    email: 'pperez@perez.com',
-    password: 'pperezs123',
-  };
   loginForm: FormGroup;
 
   @BlockUI() blockUI: NgBlockUI;
@@ -26,11 +21,27 @@ export class LoginComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit(): void {
     this.blockUI.start('Loading...');
+
+    let user: User = {
+      email: this.loginForm.get('email')?.value,
+      password: this.loginForm.get('password')?.value,
+    };
+
     this.authService
-      .login(this.user)
-      .catch((e) => console.log(e))
+      .login(user)
+      .then((e) => console.log(e))
+      .catch((e) => {
+        Swal.fire({
+          title: 'Error!',
+          text: e,
+          icon: 'error',
+          confirmButtonColor: '#fac344'
+        });
+      })
       .finally(() => this.blockUI.stop());
   }
 }
